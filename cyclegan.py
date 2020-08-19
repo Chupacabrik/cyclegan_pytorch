dfd7f878197bd048f49fb9bba12ca154fa7dfeeb
Chupacabrik <61741948+Chupacabrik@users.noreply.github.com>
2020-08-19 16:36:43 +0300

    Add files via upload

--- cyclegan.py
+++ cyclegan.py
@@ -30,7 +30,6 @@ params = {
     'fliplr':True,
     #model params
     'num_pool':50,
-    'num_epochs':100,
     'decay_epoch':100,
     'ngf':32,   #number of generator filters
     'ndf':64,   #number of discriminator filters
@@ -50,10 +49,10 @@ transform = transforms.Compose([
     transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
 ])
 
-dir_human_train = '/../photo/train'
-dir_human_test = '/../photo/test'
-dir_simpsons_train = '/../style/train'
-dir_simpsons_test = '/../style/test/'
+dir_human_train = '../photo/train'  #'/../photo/train'
+dir_human_test =  '../photo/test' #'/../photo/test'
+dir_simpsons_train = '../style/train'  #'/../style/train'
+dir_simpsons_test = '../style/test/'  #'/../style/test/'
 batch_size = 1
 train_data_A = Mydataset(dir_human_train, transform)
 train_data_loader_A = torch.utils.data.DataLoader(
@@ -74,8 +73,8 @@ test_data_loader_B = torch.utils.data.DataLoader(
 
 
 # Get specific test images
-test_real_A_data = train_data_A.__getitem__(9).unsqueeze(0) # Convert to 4d tensor (BxNxHxW)
-test_real_B_data = train_data_B.__getitem__(2).unsqueeze(0)
+test_real_A_data = train_data_A.__getitem__(0).unsqueeze(0) # Convert to 4d tensor (BxNxHxW)
+test_real_B_data = train_data_B.__getitem__(1).unsqueeze(0)
 
 #Build Model 
 G_A = Generator(3, params['ngf'], 3, params['num_resnet']).cuda() # input_dim, num_filter, output_dim, num_resnet
@@ -105,10 +104,142 @@ G_B_avg_losses = []
 cycle_A_avg_losses = []
 cycle_B_avg_losses = []
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 # Generated image pool
 fake_A_pool = ImagePool(params['num_pool'])
 fake_B_pool = ImagePool(params['num_pool'])
 
-train(opt.num_epochs, params['decay_epoch'], params['lrD'], params['lrG'], params['lambdaA'], params['lambdaB'])
+step = 0
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+for epoch in range(opt.num_epochs):
+    D_A_losses = []
+    D_B_losses = []
+    G_A_losses = []
+    G_B_losses = []
+    cycle_A_losses = []
+    cycle_B_losses = []
+
+    # Learing rate decay 
+    if(epoch + 1) > params['decay_epoch']:
+        D_A_optimizer.param_groups[0]['lr'] -= params['lrD'] / (opt.num_epochs -params['decay_epoch'])
+        D_B_optimizer.param_groups[0]['lr'] -= params['lrD'] / (opt.num_epochs - params['decay_epoch'])
+        G_optimizer.param_groups[0]['lr'] -= params['lrD'] / (opt.num_epochs - params['decay_epoch'])
+# training 
+    for i, (real_A, real_B) in enumerate(zip(train_data_loader_A, train_data_loader_B)):
+
+        # input image data
+        real_A = real_A.to(device)
+        real_B = real_B.to(device)
+
+        # -------------------------- train generator G --------------------------
+        # A --> B
+        fake_B = G_A(real_A)
+        D_B_fake_decision = D_B(fake_B)
+        G_A_loss = MSE_Loss(D_B_fake_decision, torch.ones(D_B_fake_decision.size()).cuda())
+
+        # forward cycle loss
+        recon_A = G_B(fake_B)
+        cycle_A_loss = L1_Loss(recon_A, real_A) * params['lambdaA']
+
+        # B --> A
+        fake_A = G_B(real_B)
+        D_A_fake_decision = D_A(fake_A)
+        G_B_loss = MSE_Loss(D_A_fake_decision, torch.ones(D_A_fake_decision.size()).cuda())
+
+        # backward cycle loss
+        recon_B = G_A(fake_A)
+        cycle_B_loss = L1_Loss(recon_B, real_B) * params['lambdaB']
+
+        # Back propagation
+        G_loss = G_A_loss + G_B_loss + cycle_A_loss + cycle_B_loss
+        G_optimizer.zero_grad()
+        G_loss.backward()
+        G_optimizer.step()
+
+
+        # -------------------------- train discriminator D_A --------------------------
+        D_A_real_decision = D_A(real_A)
+        D_A_real_loss = MSE_Loss(D_A_real_decision, torch.ones(D_A_real_decision.size()).cuda())
+
+        fake_A = fake_A_pool.query(fake_A)
+
+        D_A_fake_decision = D_A(fake_A)
+        D_A_fake_loss = MSE_Loss(D_A_fake_decision, torch.zeros(D_A_fake_decision.size()).cuda())
+
+        # Back propagation
+        D_A_loss = (D_A_real_loss + D_A_fake_loss) * 0.5
+        D_A_optimizer.zero_grad()
+        D_A_loss.backward()
+        D_A_optimizer.step()
+
+        # -------------------------- train discriminator D_B --------------------------
+        D_B_real_decision = D_B(real_B)
+        D_B_real_loss = MSE_Loss(D_B_real_decision, torch.ones(D_B_fake_decision.size()).cuda())
+
+        fake_B = fake_B_pool.query(fake_B)
+
+        D_B_fake_decision = D_B(fake_B)
+        D_B_fake_loss = MSE_Loss(D_B_fake_decision, torch.zeros(D_B_fake_decision.size()).cuda())
+
+        # Back propagation
+        D_B_loss = (D_B_real_loss + D_B_fake_loss) * 0.5
+        D_B_optimizer.zero_grad()
+        D_B_loss.backward()
+        D_B_optimizer.step()
+
+        # ------------------------ Print -----------------------------
+        # loss values
+        D_A_losses.append(D_A_loss.item())
+        D_B_losses.append(D_B_loss.item())
+        G_A_losses.append(G_A_loss.item())
+        G_B_losses.append(G_B_loss.item())
+        cycle_A_losses.append(cycle_A_loss.item())
+        cycle_B_losses.append(cycle_B_loss.item())
+
+        if i%100 == 0:
+            print('Epoch [%d/%d], Step [%d/%d], D_A_loss: %.4f, D_B_loss: %.4f, G_A_loss: %.4f, G_B_loss: %.4f'
+                    % (epoch+1, opt.num_epochs, i+1, len(train_data_loader_A), D_A_loss.item(), D_B_loss.item(), G_A_loss.item(), G_B_loss.item()))
+
+        step += 1
+
+    D_A_avg_loss = torch.mean(torch.FloatTensor(D_A_losses))
+    D_B_avg_loss = torch.mean(torch.FloatTensor(D_B_losses))
+    G_A_avg_loss = torch.mean(torch.FloatTensor(G_A_losses))
+    G_B_avg_loss = torch.mean(torch.FloatTensor(G_B_losses))
+    cycle_A_avg_loss = torch.mean(torch.FloatTensor(cycle_A_losses))
+    cycle_B_avg_loss = torch.mean(torch.FloatTensor(cycle_B_losses))
+
+    # avg loss values for plot
+    D_A_avg_losses.append(D_A_avg_loss.item())
+    D_B_avg_losses.append(D_B_avg_loss.item())
+    G_A_avg_losses.append(G_A_avg_loss.item())
+    G_B_avg_losses.append(G_B_avg_loss.item())
+    cycle_A_avg_losses.append(cycle_A_avg_loss.item())
+    cycle_B_avg_losses.append(cycle_B_avg_loss.item())
+
+    # Show result for test image
+    test_real_A = test_real_A_data.cuda()
+    test_fake_B = G_A(test_real_A)
+    test_recon_A = G_B(test_fake_B)
+
+    test_real_B = test_real_B_data.cuda()
+    test_fake_A = G_B(test_real_B)
+    test_recon_B = G_A(test_fake_A)
+
+    plot_train_result([test_real_A, test_real_B], [test_fake_B, test_fake_A], [test_recon_A, test_recon_B],
+                            epoch, save=True)
+    
+    # Save model checkpoints
+    torch.save(G_A.state_dict(), "saved_models/G_AB_%d.pth" % (epoch + 1))
+    torch.save(G_B.state_dict(), "saved_models/G_BA_%d.pth" % (epoch + 1))
+    torch.save(D_A.state_dict(), "saved_models/D_A_%d.pth" % (epoch + 1))
+    torch.save(D_B.state_dict(), "saved_models/D_B_%d.pth" % (epoch + 1))
+
+all_losses = pd.DataFrame()
+all_losses['D_A_avg_losses'] = D_A_avg_losses
+all_losses['D_B_avg_losses'] = D_B_avg_losses
+all_losses['G_A_avg_losses'] = G_A_avg_losses
+all_losses['G_B_avg_losses'] = G_B_avg_losses
+all_losses['cycle_A_avg_losses'] = cycle_A_avg_losses
+all_losses['cycle_B_avg_losses'] = cycle_B_avg_losses
+all_losses.to_csv('avg_losses',index=False)
+
